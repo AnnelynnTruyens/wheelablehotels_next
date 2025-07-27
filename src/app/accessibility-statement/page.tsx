6369d5b00674264fcdad07db81ac6e2dca85d225
AnnelynnTruyens <annelynn.truyens@student.arteveldehs.be>
2025-07-27 16:07:13 +0200

    accessibility + privacy pages

--- src/app/accessibility-statement/page.tsx
+++ src/app/accessibility-statement/page.tsx
@@ -1,3 +1,13 @@
 export default function Accessibility() {
-	return <div>Accessibility statement</div>;
+	return (
+		<main id="main" className="main">
+			<title>Accessibility Statement | Wheelable Hotels</title>
+			<h1>Accessibility statement</h1>
+			<p>
+				We aim to be as accessible as possible. Our accessibility screening for
+				the website is not finished yet. You will be able to find the digital
+				accessibility information here, once it's done.
+			</p>
+		</main>
+	);
 }
--- src/app/privacy-statement/page.tsx
+++ src/app/privacy-statement/page.tsx
@@ -1,3 +1,22 @@
 export default function Privacy() {
-	return <div>Privacy statement</div>;
+	return (
+		<main id="main" className="main">
+			<title>Privacy Statement | Wheelable Hotels</title>
+			<h1>Privacy statement</h1>
+			<p>We value your privacy.</p>
+			<h2>Registered users</h2>
+			<p>
+				We don't ask for your full name to be part of the platform. We only ask
+				for and store your email for login purposes. You can delete your account
+				at any moment, your data will then be deleted.
+			</p>
+			<h2>Contact form</h2>
+			<p>
+				We ask for a name and an email address on the contact form. This
+				information will be used to contact you and answer your question or
+				concern. After we answered your message, your information will be
+				removed.
+			</p>
+		</main>
+	);
 }
