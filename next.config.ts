3ec2603f4bcb90caa77b424d0674294d2ce44462
AnnelynnTruyens <annelynn.truyens@student.arteveldehs.be>
2025-07-31 20:36:59 +0200

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	webpack: (config) => {
+		config.resolve.alias["mongoose"] = require.resolve("mongoose");
+		return config;
+	},
 };
 
 export default nextConfig;
